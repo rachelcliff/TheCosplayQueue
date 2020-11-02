2cea84565cf63967891eb210e551b2fe054b73df
rachelcliff <64233317+rachelcliff@users.noreply.github.com>
2020-11-02 14:25:13 +1000

    Code comments

--- Api/session.php
+++ Api/session.php
@@ -18,6 +18,7 @@ public function getRequestCounter()
     return $this->_requestCounters;
 }
 
+// This rate limiting function starts by checking to see if there is an existing rate limiting session in place, if there is one existing it will continue to add additional requests to the request counter. If the start time is null it will set the current time as the start time. The 3600 in the equation refers to how many seconds in an hour. The rate limiting code functions by determining the $hours variable set in the previous section of the function. If the $hours variable is more than 24 and the number of requests of the page is less than 1000, it will return a false result allowing for the request counter to continue working. If the $hours variable is more than 24 (hours in a day), then the counter will reset to 0 and the rate limiting will continue. 
 public function Rate24HourCheck() {
     if ($this->_startTime == null) {
         $this->_startTime = time();
@@ -33,6 +34,21 @@ public function Rate24HourCheck() {
         $this->_requestCounter=0;
     }
     return $this->_requestCounter;
+}
+
+public function RateCheck() {
+    if ($this->_startTime == null) {
+        $this->_startTime = time();
+    }
+    $this->requestCounter++;
+    $seconds = (time() - $this->_startTime/60);
+    $this->_startTime=time();
+    if ($seconds >1 && $this ->_requestCounter <1) {
+        return false;
+    } else if ($seconds <=1) {
+    $this->_requestCounter=0;
+    }
+return $this->_requestCounter;
 }
 
 // checking if someone is already logged in. This comes from the session variable "login" that is set when someone successfully interacts with one of the 3 database facing forms on the site. If it is set to true it will return a response code of 201, and if it false it will return a response code of 401. The second half of the function lives within the api.php file.  
@@ -69,4 +85,3 @@ function logout() {
 session_destroy();
 }
 }
-?>
