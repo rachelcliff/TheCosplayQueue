d3c25bfb9d74802c53f40916522fa6b1b51b295b
rachelcliff <64233317+rachelcliff@users.noreply.github.com>
2020-11-29 23:24:26 +1000

    Added HTML Help file

--- Api/api.php
+++ Api/api.php
@@ -1,6 +1,6 @@
 <?php
 header('Access-Control-Allow-Origin: *');
-// header('Access-Control-Allow-Origin: https://localhost:3000');
+header('Access-Control-Allow-Origin: https://localhost:3000');
 header('Access-Control-Allow-Credentials: true');
 header("Access-Control-Allow-Headers", "content-type");
 header('Content-Type: application/json'); // All echo statements are json_encode
